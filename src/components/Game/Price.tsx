eb88e8fd3e2038b3de700081272cf5f75b419a42
Miraya Abrodi <mirayaabrodi@gmail.com>
2023-07-22 12:23:34 +0200

    style: Apply styling to Price component for BTC price display

--- src/components/Game/Price.tsx
+++ src/components/Game/Price.tsx
@@ -3,9 +3,15 @@ import useStore from "../../library/state-manager";
 export function Price() {
   const { btcPrice } = useStore();
   return (
-    <div>
-      <span>BTC price in USD</span>
-      <span aria-live="polite" id="bitcoin-price">
+    <div className="relative mx-auto bg-white max-w-4xl h-[180px] rounded-xl p-4 shadow-md flex justify-center mb-5 w-[400px] border">
+      <span className="absolute top-4 left-4 text-md font-medium">
+        BTC price in USD
+      </span>
+      <span
+        aria-live="polite"
+        id="bitcoin-price"
+        className="text-4xl text-center font-semibold my-auto pt-6"
+      >
         {btcPrice ? btcPrice + " USD" : "Loading..."}
       </span>
     </div>
