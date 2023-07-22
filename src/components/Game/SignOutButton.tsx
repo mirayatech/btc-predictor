3a7007695d144fc338f4d2e59af3eb1e933532d9
Miraya Abrodi <mirayaabrodi@gmail.com>
2023-07-22 12:36:28 +0200

    style: Apply CSS styling to SignOutButton component

--- src/components/Game/SignOutButton.tsx
+++ src/components/Game/SignOutButton.tsx
@@ -17,7 +17,12 @@ export default function SignOutButton() {
   }
 
   return (
-    <button onClick={signOut} aria-label="sign out">
+    <button
+      onClick={signOut}
+      aria-label="sign out"
+      style={{ textShadow: "0px -2px #d7a83b" }}
+      className="bg-[#f0be26] border-b-4 border-[#d19e28] text-white text-md ease-in duration-200 p-3 rounded-lg shadow-3xlactive:translate-y-[2px] active:border-b-2"
+    >
       Sign Out
     </button>
   );
