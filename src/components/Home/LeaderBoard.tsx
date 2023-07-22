fb084b6f4dc766cc89fa274232171ebf1848f010
Miraya Abrodi <mirayaabrodi@gmail.com>
2023-07-22 12:52:47 +0200

    feat: Create LeaderBoard component to display user scores in descending order

---
+++ src/components/Home/LeaderBoard.tsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import { UserType } from "../../library/types";
+import { Database } from "../../library/firebase/firebase";
+import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import { useAuthContext } from "../../library/firebase/firebaseAuth";
+export default function LeaderBoard() {
+  const [users, setUsers] = useState<UserType[]>([]);
+
+  const { user } = useAuthContext();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const userRef = collection(Database, "user");
+      const userQuery = query(userRef, orderBy("score", "desc"));
+      const data = await getDocs(userQuery);
+      setUsers(data.docs.map((doc) => ({ ...(doc.data() as UserType) })));
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="max-w-screen-md  mx-auto pb-20">
+      <div className="my-4 border-b-2">
+        <h2 className="text-xl font-bold leading-10">LeaderBoard</h2>
+      </div>
+      <div className="p-10 bg-[#e1e9f0] max-h-[500px] overflow-auto rounded-sm border shadow-lg">
+        {users.map((player, index) => {
+          let medal = "";
+          if (index === 0) {
+            medal = "🥇";
+          } else if (index === 1) {
+            medal = "🥈";
+          } else if (index === 2) {
+            medal = "🥉";
+          }
+          return (
+            <div key={index} className="w-full bg-white rounded  border my-3 ">
+              <div>
+                <div className="p-3">
+                  <div className="uppercase tracking-wide text-sm text-[#16a34a] font-semibold rounded-md">
+                    {medal ? (
+                      <span className="text-3xl"> {medal}</span>
+                    ) : (
+                      `#${index + 1}`
+                    )}
+                  </div>
+                  <p className="block mt-1 text-lg leading-tight font-medium text-black">
+                    {user?.uid
+                      ? user?.uid === player.id
+                        ? "You"
+                        : player.name
+                      : player.name}
+                  </p>
+                  <p className="mt-2 text-gray-500">Score: {player.score}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
