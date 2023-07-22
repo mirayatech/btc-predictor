6fafc98fc3fdeee436807db9377b357a76e2abe0
Miraya Abrodi <mirayaabrodi@gmail.com>
2023-07-22 12:51:18 +0200

    feat: created Banner component

---
+++ src/components/Home/Banner.tsx
@@ -0,0 +1,29 @@
+import Image from "../../assets/bitcoin.png";
+import { Link } from "react-router-dom";
+import { useAuthContext } from "../../library/firebase/firebaseAuth";
+
+export function Banner() {
+  const { user } = useAuthContext();
+
+  const linkUrl = user?.uid ? `/user/${user?.uid}` : `/sign-in`;
+
+  return (
+    <div className=" h-400 flex flex-col justify-center items-center gap-5 bg-[#252326] shadow-xl py-10">
+      <img src={Image} alt="Bitcoin" className="banner__image w-20 mb-5" />
+
+      <h1 className=" text-white text-3xl font-bold uppercase text-40 leading-3">
+        BTC Predictor
+      </h1>
+      <h2 className="text-white font-medium text-20">
+        Predict the Future of Bitcoin in Minutes
+      </h2>
+      <Link
+        to={linkUrl}
+        style={{ textShadow: "0px -2px #d7a83b" }}
+        className="bg-[#f3b42e] border-b-4 border-[#d79f25]   text-white text-md ease-in duration-200 p-2 px-20 mb-5 rounded  shadow-3xlactive:translate-y-[2px] active:border-b-2"
+      >
+        Play Now
+      </Link>
+    </div>
+  );
+}
