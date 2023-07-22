f2d893586b147d67c77cc1fd342b937f29a123a8
Miraya Abrodi <mirayaabrodi@gmail.com>
2023-07-22 12:53:05 +0200

    feat: Create footer component

---
+++ src/components/Home/Footer.tsx
@@ -0,0 +1,28 @@
+import { CodeIcon, GithubIcon } from "../../assets/icons";
+
+export default function Footer() {
+  return (
+    <footer className="flex justify-center flex-col gap-3 p-5 items-center bg-[#252326]">
+      <a
+        href="https://www.miraya.tech/"
+        target="_blank"
+        className="flex items-center text-[#939ea7] duration-150 ease-linear hover:text-[#b6bdc4]"
+      >
+        <span className="pr-1">
+          <GithubIcon />
+        </span>
+        Created by Miraya
+      </a>
+      <a
+        href="https://github.com/mirayatech/BTC-Predictor"
+        target="_blank"
+        className="flex items-center text-[#939ea7] duration-150 ease-linear hover:text-[#b6bdc4]"
+      >
+        <span className="pr-1">
+          <CodeIcon />
+        </span>
+        Source code on GitHub
+      </a>
+    </footer>
+  );
+}
