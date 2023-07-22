279b6b897b8bef7e4230a973a5c084c9d9333dd8
Miraya Abrodi <mirayaabrodi@gmail.com>
2023-07-22 12:39:40 +0200

    feat: Export Icon Components from react-icons and import image

---
+++ src/assets/icons.ts
@@ -0,0 +1,9 @@
+import {
+  FiLogOut as LogOutIcon,
+  FiGithub as GithubIcon,
+  FiCode as CodeIcon,
+} from "react-icons/fi";
+import { FaQuestion as QuestionIcon } from "react-icons/fa";
+import { HiMiniXMark as CloseIcon } from "react-icons/hi2";
+
+export { LogOutIcon, GithubIcon, CodeIcon, QuestionIcon, CloseIcon };
