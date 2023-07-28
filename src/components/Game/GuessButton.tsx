5197edf6c9a92a983d689031646691434f21412e
Miraya Abrodi <mirayaabrodi@gmail.com>
2023-07-28 23:49:32 +0200

    refactor: gusess button components and add more color variables

--- src/components/Game/GuessButton.tsx
+++ src/components/Game/GuessButton.tsx
@@ -40,20 +40,19 @@ export function GuessButtons() {
 
   const buttonClassesUp = `relative px-10 py-2 m-2 float-left rounded-lg text-white text-lg shadow-md ease-in duration-200 w-[150px] ${
     selectedButton === "up"
-      ? "bg-[#82BF56] text-shadow-[0px 2px 4px rgba(0, 0, 0, 0.25)]"
+      ? "bg-greenPrimary"
       : isGuessing
-      ? "bg-[#82bf56ad] border-b-4 border-[#6696446b] text-shadow-[0px 2px 4px rgba(0, 0, 0, 0.25)] pointer-events-none"
-      : "bg-[#82BF56] border-b-4 border-[#669644] text-shadow-[0px 2px 4px rgba(0, 0, 0, 0.25)]"
+      ? "bg-greenPrimary border-b-4 border-greenSecondary  pointer-events-none opacity-70"
+      : "bg-greenPrimary border-b-4 border-greenSecondary"
   } ${!isGuessing ? "active:translate-y-[3px] active:border-b-2" : ""}`;
 
   const buttonClassesDown = `relative px-10 py-2 m-2 float-left rounded-lg text-white text-lg shadow-md  ease-in duration-200 w-[150px] ${
     selectedButton === "down"
-      ? "bg-[#E74C3C] text-shadow-[0px 2px 4px rgba(0, 0, 0, 0.25)]"
+      ? "bg-redPrimary"
       : isGuessing
-      ? "bg-[#e74d3c99] border-b-4 border-[#bd3f315b] text-shadow-[0px 2px 4px rgba(0, 0, 0, 0.25)] pointer-events-none"
-      : "bg-[#E74C3C] border-b-4 border-[#BD3E31] text-shadow-[0px 2px 4px rgba(0, 0, 0, 0.25)]"
+      ? "bg-redPrimary border-b-4 border-redSecondary pointer-events-none opacity-70"
+      : "bg-redPrimary border-b-4 border-redSecondary"
   } ${!isGuessing ? "active:translate-y-[2px] active:border-b-2" : ""}`;
-
   return (
     <div className="border mx-auto bg-white w-[400px] h-[100px] rounded-xl p-4 shadow-md flex justify-between">
       <button
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,11 @@ export default {
       colors: {
         yellowPrimary: "#f3b42e",
         yellowSecondary: "#cf9822",
+        greenPrimary: "#82BF56",
+        greenSecondary: "#5e8d3d",
+        redPrimary: "#E74C3C",
+        redSecondary: "#b83a2c",
+        shadow: "[0px 2px 4px rgba(0, 0, 0, 0.25)]",
       },
     },
   },
